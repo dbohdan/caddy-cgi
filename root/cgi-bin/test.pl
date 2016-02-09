a882699b6ffd2cfbe8a6b3f6268c7ef881512c60
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-02-09 18:09:38 +0200

    More complex root/cgi-bin/test.pl

--- root/cgi-bin/test.pl
+++ root/cgi-bin/test.pl
@@ -1,6 +1,36 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
 
-print "Content-Type: text/html\n\n";
-print "<h1>Hello, World!</h1>";
+use CGI;
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+
+my $cgi = CGI->new;
+print $cgi->header('text/html');
+
+my $title = "CGI test";
+
+print << "END_HTML";
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>$title</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <h1>$title</h1>
+        <form method="post" enctype="multipart/form-data">
+            Name: <input type="text" name="name" value="Anonymous"><br>
+            Message: <input type="text" name="message"><p>
+            <input type="submit" name="Submit" value="Submit">
+        </form>
+        <hr>
+END_HTML
+
+if ((my $name = $cgi->param('name'))
+        && (my $message = $cgi->param('message'))) {
+    print "${\$cgi->escapeHTML($name)}: ${\$cgi->escapeHTML($message)}\n";
+}
+
+print '</body></html>';
